cea8cffee44a6d424a0b18ebe2562ba8871b4ced
Shu-gez <234665429@qq.com>
2022-04-18 20:57:12 +0800

    2022.04.18 Update
    2022.04.18 Update

--- code/using_pages.hpp
+++ code/using_pages.hpp
@@ -28,18 +28,24 @@ bool _VChar(char c) {
     return ((_Isletter(c)) || _Isdigit(c));
 }
 
-#include<fastio.hpp>
+#include "fastio.hpp"
 
 #define MTX_MX 100
 
-template <typename t> class Matrix{
-    private:
+template <typename t> struct Matrix{
+//    private:
         t Mtx[MTX_MX+5][MTX_MX+5];
-    public:
+//    public:
         int n,m;
         void push(int i,int j,t a) {
             Mtx[i][j]=a;
         }
+        void add(int i,int j,t a) {
+            Mtx[i][j]+=a;
+        }
+        t _Nump(int i,int j) {
+            return Mtx[i][j];
+        } 
         void out() {
             for(int i=1;i<=n;++i) {
                 for(int j=1;j<=m;++j) {
@@ -51,9 +57,18 @@ template <typename t> class Matrix{
         }
 };
 
-Matrix operator * (Matrix a,Matrix b) {
-    Matrix C;
-    for(int i=1;i<=a.)
+//I'm very sorry that due to my mistake, matrix can't give full play to its maximum performance.
+
+template <typename t> Matrix<t> operator * (Matrix<t> a,Matrix<t> b) {
+    Matrix<t> C;
+    for(int i=1;i<=a.n;++i) {
+        for(int j=1;j<=b.m;++j) {
+            C.push(i,j,0);
+            for(int k=1;k<=a.n;++k) {
+                C.add(i,j,a._Nump(i,k)+b._Nump(k,j));
+            }
+        }
+    }
 }
 
 #endif
