c586056a31e77578c4f1dfe0d0ba5690ca0da600
Shu-gez <234665429@qq.com>
2022-04-19 13:35:56 +0800

    2022.04.19 Update
    2022.04.19 Update

--- allhead.hpp
+++ allhead.hpp
@@ -1,15 +1,28 @@
 /*2022.04.19 Update 
   Author: Shuger*/
 
+#define GCC true
+#define CPP true
+
 #ifndef _ALLHEAD_HPP
 #define _ALLHEAD_HPP
 
+#ifdef CPP
+
 #include "include/using_pages.hpp"
 #include "include/math.hpp"
+
+#endif
 
 /*#define LOCAL true
 #define ONLINE_JUDGE true*/
 
 //If you want to give the header files to local computer,define LOCAL,or define ONLINE_JUDGE.
+
+#ifdef GCC
+
+#include<gcc/usp.h>
+
+#endif
 
 #endif
