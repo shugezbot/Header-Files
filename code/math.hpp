3d71acd8ab623e7d466f7c90926a4512e9cb281f
Shu-gez <234665429@qq.com>
2022-04-19 12:25:37 +0800

    2022.04.19 Update
    2022.04.19 Update

--- code/math.hpp
+++ code/math.hpp
@@ -45,7 +45,7 @@ template <typename t> t ExGcd(t a,t b,t &x,t &y) {
     }
 }
 
-template <typename t> inv(t a,t n,t Md) {
+template <typename t> t __InV(t a,t n,t Md) {
     t d,x,y;
     d=ExGcd(a,n,x,y);
     return d==Md ? (x%n+n) % n : -1;
